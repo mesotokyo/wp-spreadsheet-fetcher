10345a7a044ed765826d575760ca3bde81c4f48a
mesotokyo <meso@meso.tokyo>
2016-08-22 04:28:11 +0900

    tsv_parser: add header_row and order_reverse option

--- test/test.php
+++ test/test.php
@@ -5,11 +5,17 @@ $parser = new TSVParser();
 $opts = array(
     "start_row" => 3,
     "key_row" => 1,
-    "end_row" => 5,
+    "end_row" => 4,
+    "header_row" => 2,
+    "order_reverse" => 1,
 );
 $parser->load('./test.tsv', $opts);
 
-$template = '$$ foo:$foo bar:${bar}_$hoge $moge';
+$template = '$$ foo:$foo bar:${bar}_$moge $hoge';
 
 $result = $parser->render($template);
+//print_r($result);
+
+$result = $parser->render_header($template, 1);
 print_r($result);
+
--- tsv_parser.php
+++ tsv_parser.php
@@ -10,18 +10,39 @@ class TSVParser {
 	}
 	
 	function _parse_tsv($tsv, $opts) {
-		$rows = explode("\n", $tsv);
+		$rows = explode("\n", trim($tsv));
 		$row_size = count($rows);
+        $start_row = ($opts["start_row"] > 0) ? $opts["start_row"] - 1 : 0;
+        $end_row = ($opts["end_row"] > 0) ? $opts["end_row"] - 1 : $row_size - 1;
+
 		if ($opts["key_row"] > 0) {
 			$keys = explode("\t", trim($rows[$opts["key_row"]-1]));
 		}
+
+        if ($opts["header_row"] > 0) {
+			$values = explode("\t", trim($rows[$opts["header_row"]-1]));
+            $headers = array();
+            for ($j = 0; $j < count($values); $j++) {
+                $key = $keys[$j];
+                if ($key && $values[$j]) {
+                    $headers[$key] = $values[$j];
+                }
+            }
+            $this->headers = $headers;
+        }
+
+        $row_count = $end_row - $start_row + 1;
+        $target_rows = array_slice($rows, $start_row, $row_count);
+        if ($opts["order_reverse"]) {
+            $target_rows = array_reverse($target_rows);
+        }
+
 		$result = array();
-        $start_row = ($opts["start_row"] > 0) ? $opts["start_row"] - 1 : 0;
-		for ($i = $start_row; $i < $row_size; $i++) {
+		for ($i = 0; $i < $row_count; $i++) {
 			if ($opts["key_row"] > 0) { 
 				// use key
 				$items = array();
-				$values = explode("\t", trim($rows[$i]));
+				$values = explode("\t", trim($target_rows[$i]));
 				for ($j = 0; $j < count($values); $j++) {
 					$key = $keys[$j];
 					if ($key && $values[$j]) {
@@ -29,12 +50,16 @@ class TSVParser {
 					}
 				}
 			} else {
-				$items = explode("\t", $rows[$i]);
+				$items = explode("\t", $target_rows[$i]);
 			}
 			array_push($result, $items);
 		}
 		$this->parsed = $result;
 	}
+
+	function render_header($template) {
+        return $this->_render($this->headers, $template);
+    }
 
 	function render($template) {
 		$result = array();
